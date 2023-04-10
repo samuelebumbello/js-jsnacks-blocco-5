e213967cb4380293f1b88d81841619e7a28a4a89
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-10 21:53:25 +0200

    [add] html and array object js

---
+++ jsnack-5/js/script.js
@@ -0,0 +1,35 @@
+const biciCorsa = [
+    {
+        nome: "sportiva",
+        peso: 15,
+    },
+    {
+        nome: "cross",
+        peso: 11,
+    },
+    {
+        nome: "passeggio",
+        peso: 21,
+    },
+    {
+        nome: "montagna",
+        peso: 18,
+    },
+    {
+        nome: "stradale",
+        peso: 16,
+    },
+    {
+        nome: "ruote grosse",
+        peso: 23,
+    },
+    {
+        nome: "ruote sottili",
+        peso: 28,
+    },
+    {
+        nome: "senza marcia",
+        peso: 10,
+    },
+];
+
