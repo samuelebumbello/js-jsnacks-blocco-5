aae7f29602ff4668a6346346e34c8e4bd0cb54e2
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-10 22:37:14 +0200

    [add] html and array object js

---
+++ jsnack-4/js/script.js
@@ -0,0 +1,37 @@
+const students = [
+    {
+    id:125,
+    nome: "Marco rossi",
+    sommaVoti: 72,
+    },
+    {
+    id: 187, 
+    nome: "Riccardo Mastro",
+    sommaVoti: 95,
+    },
+    {
+    id: 721,
+    nome: "Alessandro Aversa",
+    sommaVoti: 85,
+    },
+    {
+    id: 891,
+    nome: "Teresa Sanf",
+    sommaVoti: 99,
+    },
+    {
+    id: 174,
+    nome: "Luigi Gambino",
+    sommaVoti: 45,
+    },
+    {
+    id: 987,
+    nome: "Fabrizio Pinguino",
+    sommaVoti: 84,
+    },
+    {
+    id: 123,
+    nome: "Giuseppe Crocchetta",
+    sommaVoti: 78,
+    }
+];
